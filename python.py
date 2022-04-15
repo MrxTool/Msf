ea18628bde44354e17c26e0e03bbc6a408812265
MrxTools <103169446+MrxTool@users.noreply.github.com>
2022-04-15 22:03:17 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+#Msf
+print('\033[91m'+ '\033[1m''Before start please click README.md :)')
+
+print('* Tools Metasploit')
+print('* Target')
+print('* Hacking')
+print('* Deface')
+print('* DDOS')
+print('-----------')
+print('-help     -nmap   -n   -u --nano -bash')
+
+raw_input('Select: '+'\033[91m'+'\033[0m')
